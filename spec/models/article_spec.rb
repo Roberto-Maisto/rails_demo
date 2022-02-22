0ba77e2e72c2ffdd27a6d0a9e57b8a29bb6022f7
Roberto <maisto_roberto@hotmail.it>
2022-02-22 17:57:33 +0100

    Adds Factory to Article test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,8 +1,12 @@
-require 'rails_helper'
-
 RSpec.describe Article, type: :model do
-  describe 'database' do
-    it { is_expected.to have_db_column(:title).of_type(:string)}
-    it { is_expected.to have_db_column(:body).of_type(:text)}
+  describe 'Database' do
+    it { is_expected.to have_db_column(:title).of_type(:string) }
+    it { is_expected.to have_db_column(:body).of_type(:text) }
+  end
+
+  describe 'Factory' do
+    it 'is expected to be valid' do
+      expect(create(:article)).to be_valid
+    end
   end
 end
